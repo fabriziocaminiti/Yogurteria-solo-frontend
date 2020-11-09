ba9e5eb51173cd337d37a41ccbaf31f4f87b463a
Fabrizio Caminiti <fabriziocaminiti@Air-di-Fabrizio.home-life.hub>
2020-11-09 14:41:21 +0100

    fix responsive navbar e form contatto

--- script.js
+++ script.js
@@ -6,16 +6,15 @@ let sezioni2 = document.querySelector('#testoNav2')
 let sezioni3 = document.querySelector('#testoNav3')
 
 
-if (window.pageYOffset>120){
-    navbar.classList.add('bg-light','shadow')
+if (window.pageYOffset > 20){
+    navbar.classList.add('bg-white','shadow')
     sezioni.classList.add('text-dark') 
     sezioni2.classList.add('text-dark') 
     sezioni3.classList.add('text-dark') 
 } else {
-     navbar.classList.remove('bg-light','shadow')  
+     navbar.classList.remove('bg-white','shadow')  
      sezioni.classList.remove('text-white')  
      sezioni2.classList.remove('text-white')  
      sezioni3.classList.remove('text-white')  
 }
-console.log(window.pageYOffset)
 })
